bca6c14de0ab006c130b023f4c36d96e33322d8c
pmaher-mamori-io <85150710+pmaher-mamori-io@users.noreply.github.com>
2025-10-30 09:44:48 +1100

    Update for lockouts

--- rules/failed_connection_handler.js
+++ rules/failed_connection_handler.js
@@ -1,30 +1,46 @@
-//LOGGER.info("***** CONNECTION FAILED HANDLER - "+facts.toJSON());
+//LOGGER.info("failed_connection_handler facts: {}", facts.toJSON());
 let failReason = facts.get('login_status_description');
-if (failReason && failReason.includes('invalid source IP')){
+if (failReason && failReason.includes('invalid source IP')) {
   mamori.alert("default_invalid_service_account_access", facts);  
-} else {
-    var cID = facts.get('connectionId');
-    var username = facts.get("username");
-    var SQL = "select event, count(*) count from SYS.CONNECTION_EVENTS where connection_id = "+cID+" and (lower(event) like '%null%' or lower(event) like '%invalid%' or lower(event) like '%password: login failed%' ) group by event";
-    var rs = mamori.query(SQL);
+} 
+else {
+    const cID = facts.get('connectionId');
+    const username = facts.get("username");
+    const lockOut = facts.get('lockout_expiry') ;
+   
+    const conditions = "LOWER(event) LIKE '%null%'" +
+                " OR LOWER(event) LIKE '%invalid%'" + 
+                " OR LOWER(event) LIKE '%authentication timed out%'" + 
+                " OR LOWER(event) LIKE '%denied mfa request%'" + 
+                " OR LOWER(event) LIKE '%password: login failed%'"; 
+    const SQL = "SELECT event, COUNT(*) count FROM SYS.CONNECTION_EVENTS WHERE connection_id = " + cID + " AND (" + conditions + ") GROUP BY event";
+    const rs = mamori.query(SQL);
     try {
-      while(rs.next()) {
-         facts.set('error',rs.get('event'));
-         facts.set('subject',"Mamori Alert! User "+username+" has a failed login to Target Resource : "+facts.get('system')+" due to "+facts.get('error')+".");
-         var msg = "<br>User: "+username+"<br>"+
-                   "Client: "+facts.get('ip')+"<br>"+
-                   "Target Resource: "+facts.get('system')+"<br>"+
-                   "Protocol: "+facts.get('clientProtocol')+"<br>"+
-                   "Error: "+facts.get('error');
-
-         facts.set('text',msg);     
-         facts.set('format',"html");
-         //LOGGER.info("User Failed ConnectionFound: " + facts.toJSON());
-         mamori.alert("alert_user", facts);
-      }
+         while (rs.next()) {
+           facts.set('error', rs.get('event'));
+           
+           if (!lockOut) {
+              facts.set('subject', "Mamori Alert! User " + username + " has a failed login to Target Resource: " + facts.get('system') 
+                        + " due to " + facts.get('error') + ".");
+           } 
+           else {
+              facts.set('subject', "Mamori Alert! User " + username + " - Account is locked until: " + lockOut);
+           }
+           
+           var msg = "<br>User: " + username + "<br>" +
+                     "Client: " + facts.get('ip') + "<br>" +
+                     "Target Resource: " + facts.get('system') + "<br>" +
+                     "Protocol: " + facts.get('clientProtocol') + "<br>" +
+                     "Error: " + facts.get('error') + 
+        			  (!lockOut ? "" : "<br>Locked Out Until: " + lockOut);
+           facts.set('text', msg);     
+           facts.set('format', "html");
+           
+           mamori.alert("default_alert_user", facts);
+        }     
     } 
     catch (e) {
-        LOGGER.info("Exception : connection_failed_handler {}",e);
+        LOGGER.info("Exception in failed_connection_handler event handler", e);
     } 
     finally {
         rs.close();
